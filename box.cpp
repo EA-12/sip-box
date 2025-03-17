3795ff2237dc2c35929be394e69fdba37dab5e1c
E-12 <evalcaide@gmail.com>
2025-03-17 17:53:46 +0100

    feat(MovimientoEscena): implementa click and drag para mover la escena, pero el scroll no funciona bien

--- box.cpp
+++ box.cpp
@@ -5,14 +5,6 @@
 using namespace chai3d;
 using namespace std;
 
-// Global Variables
-cWorld* world;
-cCamera* camera;
-cDirectionalLight* light;
-
-// GLFW Window
-GLFWwindow* window;
-
 //------------------------------------------------------------------------------
 // STATES
 //------------------------------------------------------------------------------
@@ -22,13 +14,33 @@ enum MouseStates
     MOUSE_MOVE_CAMERA
 };
 
+//------------------------------------------------------------------------------
+// DECLARED VARIABLES
+//------------------------------------------------------------------------------
+cWorld* world;
+cCamera* camera;
+cDirectionalLight* light;
+GLFWwindow* window;
+// mouse state
+MouseStates mouseState = MOUSE_IDLE;
+
+// last mouse position
+double mouseX, mouseY;
+
+//------------------------------------------------------------------------------
+// DECLARED FUNCTIONS
+//------------------------------------------------------------------------------
+// callback to handle mouse click
+void onMouseButtonCallback(GLFWwindow* a_window, int a_button, int a_action, int a_mods);
+
+// callback to handle mouse motion
+void onMouseMotionCallback(GLFWwindow* a_window, double a_posX, double a_posY);
+
+// callback to handle mouse scroll
+void onMouseScrollCallback(GLFWwindow* a_window, double a_offsetX, double a_offsetY);
+
 // Callback to render scene
-void updateGraphics()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    camera->renderView(800, 600);
-    glfwSwapBuffers(window);
-}
+void updateGraphics();
 
 // Función para cargar un modelo y crear un contenedor de mallas múltiples
 cMultiMesh* loadModel(const std::string& filepath)
@@ -61,6 +73,15 @@ int main(int argc, char* argv[])
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    // set GLFW mouse scroll callback
+    glfwSetScrollCallback(window, onMouseScrollCallback);
+
+    // set GLFW mouse position callback
+    glfwSetCursorPosCallback(window, onMouseMotionCallback);
+
+    // set GLFW mouse button callback
+    glfwSetMouseButtonCallback(window, onMouseButtonCallback);
 
     // Create a new world
     world = new cWorld();
@@ -131,4 +152,60 @@ int main(int argc, char* argv[])
     glfwTerminate();
 
     return 0;
+}
+
+void updateGraphics()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    camera->renderView(800, 600);
+    glfwSwapBuffers(window);
+}
+
+void onMouseScrollCallback(GLFWwindow* a_window, double a_offsetX, double a_offsetY)
+{
+    double r = camera->getSphericalRadius();
+    double zoomSpeed = 0.1;
+    r -= zoomSpeed * a_offsetY;  // Restar para acercar con scroll arriba, alejar con scroll abajo
+    // Limitar el rango del radio para evitar valores extremos
+    r = cClamp(r, 0.2, 10.0);
+    // Establecer el nuevo radio en la cámara
+    camera->setSphericalRadius(r);
+}
+
+void onMouseButtonCallback(GLFWwindow* a_window, int a_button, int a_action, int a_mods)
+{
+    if (a_button == GLFW_MOUSE_BUTTON_LEFT && a_action == GLFW_PRESS)
+    {
+        // store mouse position
+        glfwGetCursorPos(a_window, &mouseX, &mouseY);
+
+        // update mouse state
+        mouseState = MOUSE_MOVE_CAMERA;
+    }
+
+    else
+    {
+        // update mouse state
+        mouseState = MOUSE_IDLE;
+    }
+}
+
+void onMouseMotionCallback(GLFWwindow* a_window, double a_posX, double a_posY)
+{
+    if (mouseState == MOUSE_MOVE_CAMERA)
+    {
+        // compute mouse motion
+        int dx = a_posX - mouseX;
+        int dy = a_posY - mouseY;
+        mouseX = a_posX;
+        mouseY = a_posY;
+
+        // compute new camera angles
+        double azimuthDeg = camera->getSphericalAzimuthDeg() - 0.5 * dx;
+        double polarDeg = camera->getSphericalPolarDeg() - 0.5 * dy;
+
+        // assign new angles
+        camera->setSphericalAzimuthDeg(azimuthDeg);
+        camera->setSphericalPolarDeg(polarDeg);
+    }
 }
